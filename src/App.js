1cc97fece087fe4f281f1afa11bc85f862d06956
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-05-14 21:19:02 -0500

    feat: Made login and register route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
-
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Login from './components/LoginComponent'
+import Register from './components/RegisterComponent'
 
 import './App.css';
 
@@ -24,23 +25,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
         
+      
       </header>
       <Router>
         <Route path="/" exact component={Home}/>
         <Route path="/about" component={About}/>
+        <Route path="/login" component={Login}/>
+        <Route path="register" component={Register}/>
       </Router>
     </div>
   );
