cc382c1bdaef797392b3f0a79c84d4e2d23f7f12
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-05-28 20:12:58 -0500

    feat: Basic utilities added

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HomeComponent = () => {
     return (
-        <section>
+        <section className="center-content small-content">
             <h2>This is the Homepage</h2>
             <p>This is where we explain the benefits of the site. And how it works</p>
             <p>This will be for people who are not logged in or anything like that.</p>
