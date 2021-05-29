40fa5f65b8b9f77938561fdaf95c29ebca3a54b3
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-05-28 19:56:14 -0500

    feat: Fixed navbar styles issue

--- src/components/utilities/Navbar.js
+++ src/components/utilities/Navbar.js
@@ -4,7 +4,7 @@ import '../../navbar.css';
 
 const Navbar = () => {
     return (
-        <section>
+        <section className="navbar-comp">
             <div>The Navbar</div>
             <div className="link-wrapper">
                 <NavLink to="/">Home</NavLink>
