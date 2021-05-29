4ed2654ccab0c8c40fb293aae21e555f31440ced
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-05-28 19:48:42 -0500

    feat: Added to Home

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export default function HomeComponent() {
+const HomeComponent = () => {
     return (
-        <div>
+        <section>
             <h2>This is the Homepage</h2>
-        </div>
+            <p>This is where we explain the benefits of the site. And how it works</p>
+            <p>This will be for people who are not logged in or anything like that.</p>
+            <p>Explain mission statement, what it is and what it isn't</p>
+        </section>
     )
 }
+
+export default HomeComponent
